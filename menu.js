2503ef66b59b76b10509c26719a9980a09fbd377
Caio Berezowski <31741021+caioberezowski@users.noreply.github.com>
2021-09-20 17:25:31 -0300

    finalizando

--- menu.js
+++ menu.js
@@ -2,11 +2,12 @@ let show = true;
 
 const menuSection = document.querySelector(".menu-section")
 const menuToggle = menuSection.querySelector(".menu-toggle")
+const menuMobile = menuSection.querySelector(".menu-toggle")
 
-menuToggle.addEventListener("click", () => {
-
-    document.body.style.overflow = show ? "hidden" : "initial"
+menuSection.addEventListener("click", () => {
 
     menuSection.classList.toggle("on", show)
     show = !show;
 })
+
+
---
+++ scripts.js
@@ -0,0 +1,3 @@
+$('nav a').click(function(e)){
+  this.scrollIntoView({ behavior: "smooth" });
+}
